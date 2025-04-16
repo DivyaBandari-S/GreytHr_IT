0fcc164f9993598706c605b2624fd222af734434
sssreddys <saragada566@gmail.com>
2025-04-16 15:28:06 +0530

    exception handling issues resolved

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,5 +1,7 @@
 @extends('errors::minimal')
-
 @section('title', __('Not Found'))
 @section('code', '404')
-@section('message', __('Not Found'))
+@section('message', __('We are sorry. the page you requested was not found'))
+@section('image')
+    <img src="{{ asset('images/404-errors.jpg') }}" alt="404 Error" width="500">
+@endsection
--- resources/views/errors/500.blade.php
+++ resources/views/errors/500.blade.php
@@ -1,5 +1,7 @@
-@extends('errors::minimal')
-
-@section('title', __('Server Error'))
+@extends('errors.minimal')
+@section('title', __('Database Connection Error'))
 @section('code', '500')
-@section('message', __('Server Error'))
+@section('message', __('Database Connection Error Occurred please try again later'))
+@section('image')
+    <img src="{{ asset('images/database-connection.png') }}" alt="Database Error" width="400">
+@endsection
